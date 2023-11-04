97c9b0340d3c97c30dc70337604485f8ca10b87e
Manoj L <manoj@tagtes.com>
2023-11-04 18:56:30 +0530

    first commit

---
+++ src/Routers/Route.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Welcome from "../Pages/Welcome";
+import SignUp from "../Pages/SignUp";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import SignIn from "../Pages/SignIn";
+
+
+const Home = () => {
+  return (
+    <>
+      <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+          </Routes>
+          <Footer />
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default Home;
