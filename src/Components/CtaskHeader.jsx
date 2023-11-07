923103c0722458ca566b26597a66bba44820a439
Manoj L <manoj@tagtes.com>
2023-11-07 18:37:40 +0530

    added code in CtaskHeader & CtaskDashboard

--- src/Components/CtaskHeader.jsx
+++ src/Components/CtaskHeader.jsx
@@ -7,11 +7,68 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Image from 'react-bootstrap/Image';
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ChronoTask from '../Assets/images/ChronoTask.png';
 import ProfilePhoto from '../Assets/images/profile.jpg';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faQuestion, faBell, faGear } from "@fortawesome/free-solid-svg-icons";
+import {
+  faQuestion,
+  faBell,
+  faGear,
+  faPenToSquare,
+  faPlus,
+  faUserGroup,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+
+
+const popover = (
+  <Popover id="popover-basic">
+    <Popover.Header as="h3" style={{fontSize: '14px'}}>Account</Popover.Header>
+    <Popover.Body>
+      <div
+        className="d-flex align-items-center"
+        style={{ padding: "5px", borderRadius: "5px" }}
+      >
+        <Image
+          src={ProfilePhoto}
+          roundedCircle
+          style={{ width: "50px", height: "50px", cursor: "pointer" }}
+        />
+        <div style={{ marginLeft: "10px" }}>
+          <Row>
+            <Col>Manoj L</Col>
+          </Row>
+          <Row>
+            <Col>manoj@tagcs.in</Col>
+          </Row>
+        </div>
+      </div>
+      <div className="d-grid gap-2 mt-2">
+        <Button size="sm" style={{ backgroundColor: "#f1482f", border: 0 }}>
+          <FontAwesomeIcon icon={faPenToSquare} style={{marginRight: '2px'}}/>
+          Manage Account
+        </Button>
+      </div>
+        <div className='mt-2 mb-2'>
+          <NavDropdown.Item href="#action/3.1" style={{padding: '5px'}}>Profile</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2" style={{padding: '5px'}}>Personal Setting</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3" style={{padding: '5px'}}>Theam's</NavDropdown.Item>
+        </div>
+        <div className="d-grid gap-2">
+        <Button size="sm" style={{ backgroundColor: "#385a64", border: 0 }}>
+          <FontAwesomeIcon icon={faRightFromBracket} style={{marginRight: '2px'}}/>
+          Sign Out
+        </Button>
+      </div>
+    </Popover.Body>
+  </Popover>
+);
 
 const Header = () => {
 
@@ -61,7 +118,17 @@ const Header = () => {
             </NavDropdown>
 
             <NavDropdown title="Projects" id="collapsible-nav-dropdown">
-            <p style={{marginLeft: '15px', fontSize: '12px', marginBottom: '10px', marginTop: '10px', fontWeight: 'bolder'}}>RECENT</p>
+              <p
+                style={{
+                  marginLeft: "15px",
+                  fontSize: "12px",
+                  marginBottom: "10px",
+                  marginTop: "10px",
+                  fontWeight: "bolder",
+                }}
+              >
+                RECENT
+              </p>
               <NavDropdown.Item href="#action/3.1">
                 Account Management System
               </NavDropdown.Item>
@@ -73,7 +140,9 @@ const Header = () => {
               </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">View All</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Create Project</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">
+                Create Project
+              </NavDropdown.Item>
             </NavDropdown>
 
             <NavDropdown title="Filters" id="collapsible-nav-dropdown">
@@ -81,56 +150,119 @@ const Header = () => {
                 View Completed
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                View Pending 
+                View Pending
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">
                 View On Progress
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">View all issue</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">
+                View all issue
+              </NavDropdown.Item>
             </NavDropdown>
 
             <NavDropdown title="Teams" id="collapsible-nav-dropdown">
-            <p style={{marginLeft: '15px', fontSize: '12px', marginBottom: '10px', marginTop: '10px', fontWeight: 'bolder'}}>YOUR COLLABORATORS</p>
+              <p
+                style={{
+                  marginLeft: "15px",
+                  fontSize: "12px",
+                  marginBottom: "10px",
+                  marginTop: "10px",
+                  fontWeight: "bolder",
+                }}
+              >
+                YOUR COLLABORATORS
+              </p>
               <NavDropdown.Item href="#action/3.1">
-                Account Management System
+                <Image
+                  src="https://cdn-icons-png.flaticon.com/128/195/195528.png?ga=GA1.1.252936203.1681415593"
+                  roundedCircle
+                  style={{ width: "35px", height: "35px", cursor: "pointer" }}
+                />
+                &nbsp;Jaikumar
               </NavDropdown.Item>
+
               <NavDropdown.Item href="#action/3.2">
-                Learning Management System
+                <Image
+                  src="https://cdn-icons-png.flaticon.com/128/195/195545.png?ga=GA1.1.252936203.1681415593"
+                  roundedCircle
+                  style={{ width: "35px", height: "35px", cursor: "pointer" }}
+                />
+                &nbsp;Gokul
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">
-                Freshers Tasks
+                <Image
+                  src="https://cdn-icons-png.flaticon.com/128/195/195562.png?ga=GA1.1.252936203.1681415593"
+                  roundedCircle
+                  style={{ width: "35px", height: "35px", cursor: "pointer" }}
+                />
+                &nbsp;Sriharinivas
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">View All</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Create Project</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4"><FontAwesomeIcon icon={faPlus} /> &nbsp; &nbsp; Invite People to ChronoTask</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4"><FontAwesomeIcon icon={faUserGroup} style={{width: '15px', height:'15px'}} /> &nbsp; &nbsp; Create a Team</NavDropdown.Item>
             </NavDropdown>
 
-            <Button style={{ backgroundColor: "#385a64", border: 0 }} className='m-1'>
+            <Button
+              style={{ backgroundColor: "#385a64", border: 0 }}
+              className="m-1"
+            >
               Create
             </Button>
-
           </Nav>
           <Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-          </Form>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+            </Form>
 
-          <div className="d-flex align-items-center justify-content-center m-1">
-                <FontAwesomeIcon  icon={faBell}  style={{transform: 'rotate(30deg)', color: '#385a64', width: '20px', height: '20px', marginRight: '20px'}}/>
-          
-
-                <FontAwesomeIcon  icon={faQuestion} style={{color: '#385a64', width: '20px', height: '20px', marginRight: '20px'}}/>
-           
-
-                <FontAwesomeIcon  icon={faGear} style={{color: '#385a64', width: '20px', height: '20px', marginRight: '20px'}}/>
-         
-                <Image src={ProfilePhoto} roundedCircle style={{width: '35px', height: '35px', cursor: 'pointer'}}/>
+            <div className="d-flex align-items-center justify-content-center m-1">
+              <FontAwesomeIcon
+                icon={faBell}
+                style={{
+                  transform: "rotate(30deg)",
+                  color: "#385a64",
+                  width: "20px",
+                  height: "20px",
+                  marginRight: "20px",
+                  cursor: "pointer",
+                }}
+              />
+              <FontAwesomeIcon
+                icon={faQuestion}
+                style={{
+                  color: "#385a64",
+                  width: "20px",
+                  height: "20px",
+                  marginRight: "20px",
+                  cursor: "pointer",
+                }}
+              />
+              <FontAwesomeIcon
+                icon={faGear}
+                style={{
+                  color: "#385a64",
+                  width: "20px",
+                  height: "20px",
+                  marginRight: "20px",
+                  cursor: "pointer",
+                }}
+              />
+              <OverlayTrigger
+                trigger="click"
+                placement="bottom"
+                overlay={popover}
+              >
+                <Image
+                  src={ProfilePhoto}
+                  roundedCircle
+                  style={{ width: "35px", height: "35px", cursor: "pointer" }}
+                />
+              </OverlayTrigger>
             </div>
           </Nav>
         </Navbar.Collapse>
--- src/Pages/CtaskDashboard.jsx
+++ src/Pages/CtaskDashboard.jsx
@@ -1,12 +1,85 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import '../Assets/css/Welcome.css'
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 
 const Welcome = () => {
 
   return (
     <Container fluid>
+       <Row style={{paddingLeft: '35px', paddingRight: '35px', marginTop:'30px'}}>
+        <Col lg={12}>
+          <h1 style={{fontSize: '1.71429em'}}>Your Works</h1>
+          <hr></hr>
+        </Col>
+      </Row>
+
+      <Row style={{paddingLeft: '35px', paddingRight: '35px'}}>
+
+      <div className="d-flex justify-content-between mb-3">
+        <h6 style={{ marginRight: '10px' }}>Recent Projects</h6>
+        <Button style={{ backgroundColor: "#385a64", border: 0 }} size='sm' > View All </Button>
+      </div>
+
+        <Col xs={12} md={6} lg={4} className='mb-3'>
+          <Card>
+            <Card.Header>Quote</Card.Header>
+            <Card.Body>
+              <blockquote className="blockquote mb-0">
+                <p>
+                  {' '}
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                  posuere erat a ante.{' '}
+                </p>
+                <footer className="blockquote-footer">
+                  Someone famous in <cite title="Source Title">Source Title</cite>
+                </footer>
+              </blockquote>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col xs={12} md={6} lg={4} className='mb-3'>
+          <Card>
+            <Card.Header>Quote</Card.Header>
+            <Card.Body>
+              <blockquote className="blockquote mb-0">
+                <p>
+                  {' '}
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                  posuere erat a ante.{' '}
+                </p>
+                <footer className="blockquote-footer">
+                  Someone famous in <cite title="Source Title">Source Title</cite>
+                </footer>
+              </blockquote>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col xs={12} md={6} lg={4} className='mb-3'>
+          <Card>
+            <Card.Header>Quote</Card.Header>
+            <Card.Body>
+              <blockquote className="blockquote mb-0">
+                <p>
+                  {' '}
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+                  posuere erat a ante.{' '}
+                </p>
+                <footer className="blockquote-footer">
+                  Someone famous in <cite title="Source Title">Source Title</cite>
+                </footer>
+              </blockquote>
+            </Card.Body>
+          </Card>
+        </Col>
+        <hr></hr>
+      </Row>
 
     </Container>
   );
