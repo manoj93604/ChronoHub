fb8a99915dc13d60fadb7b400a963fc223792498
manoj647 <73732134+manoj647@users.noreply.github.com>
2023-11-07 22:23:45 +0530

    commit from main

--- src/Components/CtaskHeader.jsx
+++ src/Components/CtaskHeader.jsx
@@ -7,11 +7,55 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Image from 'react-bootstrap/Image';
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Popover from "react-bootstrap/Popover";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ChronoTask from '../Assets/images/ChronoTask.png';
 import ProfilePhoto from '../Assets/images/profile.jpg';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faQuestion, faBell, faGear } from "@fortawesome/free-solid-svg-icons";
+import {
+  faQuestion,
+  faBell,
+  faGear,
+  faPenToSquare,
+} from "@fortawesome/free-solid-svg-icons";
+
+const popover = (
+  <Popover id="popover-basic">
+    <Popover.Header as="h3">Profile Settings</Popover.Header>
+    <Popover.Body>
+      <div
+        className="d-flex align-items-center"
+        style={{ padding: "5px", borderRadius: "5px" }}
+      >
+        <Image
+          src={ProfilePhoto}
+          roundedCircle
+          style={{ width: "50px", height: "50px", cursor: "pointer" }}
+        />
+        <div style={{ marginLeft: "10px" }}>
+          <Row>
+            <Col>Manoj L</Col>
+          </Row>
+          <Row>
+            <Col>manoj@tagcs.in</Col>
+          </Row>
+        </div>
+      </div>
+      <div className="d-grid gap-2">
+        <Button size="sm" style={{ backgroundColor: "#f1482f", border: 0 }}>
+          <FontAwesomeIcon icon={faPenToSquare} style={{marginRight: '2px'}}/>
+          Edit
+        </Button>
+      </div>
+      And here's some <strong>amazing</strong> content. It's very engaging.
+      right?
+    </Popover.Body>
+  </Popover>
+);
 
 const Header = () => {
 
@@ -61,7 +105,17 @@ const Header = () => {
             </NavDropdown>
 
             <NavDropdown title="Projects" id="collapsible-nav-dropdown">
-            <p style={{marginLeft: '15px', fontSize: '12px', marginBottom: '10px', marginTop: '10px', fontWeight: 'bolder'}}>RECENT</p>
+              <p
+                style={{
+                  marginLeft: "15px",
+                  fontSize: "12px",
+                  marginBottom: "10px",
+                  marginTop: "10px",
+                  fontWeight: "bolder",
+                }}
+              >
+                RECENT
+              </p>
               <NavDropdown.Item href="#action/3.1">
                 Account Management System
               </NavDropdown.Item>
@@ -73,7 +127,9 @@ const Header = () => {
               </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">View All</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Create Project</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">
+                Create Project
+              </NavDropdown.Item>
             </NavDropdown>
 
             <NavDropdown title="Filters" id="collapsible-nav-dropdown">
@@ -81,56 +137,121 @@ const Header = () => {
                 View Completed
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                View Pending 
+                View Pending
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">
                 View On Progress
               </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">View all issue</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">
+                View all issue
+              </NavDropdown.Item>
             </NavDropdown>
 
             <NavDropdown title="Teams" id="collapsible-nav-dropdown">
-            <p style={{marginLeft: '15px', fontSize: '12px', marginBottom: '10px', marginTop: '10px', fontWeight: 'bolder'}}>YOUR COLLABORATORS</p>
+              <p
+                style={{
+                  marginLeft: "15px",
+                  fontSize: "12px",
+                  marginBottom: "10px",
+                  marginTop: "10px",
+                  fontWeight: "bolder",
+                }}
+              >
+                YOUR COLLABORATORS
+              </p>
               <NavDropdown.Item href="#action/3.1">
-                Account Management System
+                <Image
+                  src="https://cdn-icons-png.flaticon.com/128/195/195528.png?ga=GA1.1.252936203.1681415593"
+                  roundedCircle
+                  style={{ width: "35px", height: "35px", cursor: "pointer" }}
+                />
+                &nbsp;Jaikumar
               </NavDropdown.Item>
+
               <NavDropdown.Item href="#action/3.2">
-                Learning Management System
+                <Image
+                  src="https://cdn-icons-png.flaticon.com/128/195/195545.png?ga=GA1.1.252936203.1681415593"
+                  roundedCircle
+                  style={{ width: "35px", height: "35px", cursor: "pointer" }}
+                />
+                &nbsp;Gokul
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">
-                Freshers Tasks
+                <Image
+                  src="https://cdn-icons-png.flaticon.com/128/195/195562.png?ga=GA1.1.252936203.1681415593"
+                  roundedCircle
+                  style={{ width: "35px", height: "35px", cursor: "pointer" }}
+                />
+                &nbsp;Sriharinivas
               </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">View All</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.4">Create Project</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.4">
+                Create Team
+              </NavDropdown.Item>
             </NavDropdown>
 
-            <Button style={{ backgroundColor: "#385a64", border: 0 }} className='m-1'>
+            <Button
+              style={{ backgroundColor: "#385a64", border: 0 }}
+              className="m-1"
+            >
               Create
             </Button>
-
           </Nav>
           <Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-          </Form>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+            </Form>
 
-          <div className="d-flex align-items-center justify-content-center m-1">
-                <FontAwesomeIcon  icon={faBell}  style={{transform: 'rotate(30deg)', color: '#385a64', width: '20px', height: '20px', marginRight: '20px'}}/>
-          
-
-                <FontAwesomeIcon  icon={faQuestion} style={{color: '#385a64', width: '20px', height: '20px', marginRight: '20px'}}/>
-           
-
-                <FontAwesomeIcon  icon={faGear} style={{color: '#385a64', width: '20px', height: '20px', marginRight: '20px'}}/>
-         
-                <Image src={ProfilePhoto} roundedCircle style={{width: '35px', height: '35px', cursor: 'pointer'}}/>
+            <div className="d-flex align-items-center justify-content-center m-1">
+              <FontAwesomeIcon
+                icon={faBell}
+                style={{
+                  transform: "rotate(30deg)",
+                  color: "#385a64",
+                  width: "20px",
+                  height: "20px",
+                  marginRight: "20px",
+                  cursor: "pointer",
+                }}
+              />
+              <FontAwesomeIcon
+                icon={faQuestion}
+                style={{
+                  color: "#385a64",
+                  width: "20px",
+                  height: "20px",
+                  marginRight: "20px",
+                  cursor: "pointer",
+                }}
+              />
+              <FontAwesomeIcon
+                icon={faGear}
+                style={{
+                  color: "#385a64",
+                  width: "20px",
+                  height: "20px",
+                  marginRight: "20px",
+                  cursor: "pointer",
+                }}
+              />
+              <OverlayTrigger
+                trigger="click"
+                placement="bottom"
+                overlay={popover}
+              >
+                <Image
+                  src={ProfilePhoto}
+                  roundedCircle
+                  style={{ width: "35px", height: "35px", cursor: "pointer" }}
+                />
+              </OverlayTrigger>
             </div>
           </Nav>
         </Navbar.Collapse>
